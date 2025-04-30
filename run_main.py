c78ca15581487cc2b8d92bd0c7ecfb15cdec46a7
prashanta poudel <93674307+Prashantapoudel@users.noreply.github.com>
2025-04-30 15:22:17 -0400

    Update run_main.py

--- run_main.py
+++ run_main.py
@@ -1,5 +1,8 @@
 from main_app.main_app import main_app  # Explicitly import from main_app
 import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     main_app.run(host="0.0.0.0", port=port, debug=True)
