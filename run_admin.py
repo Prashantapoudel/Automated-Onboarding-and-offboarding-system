934ade4ae95fb052cc06fd2e8f94a2bd4286defc
prashanta poudel <93674307+Prashantapoudel@users.noreply.github.com>
2025-04-30 15:07:41 -0400

    Update run_admin.py

--- run_admin.py
+++ run_admin.py
@@ -1,4 +1,6 @@
 from admin_app.admin import admin_app
+import os 
 
 if __name__ == "__main__":
-    admin_app.run(port=5001, debug=True)
+    port = int(os.environ.get("PORT", 5001))  # fallback to 5000 if PORT not set
+    admin_app.run(host="0.0.0.0", port=port, debug=True)
